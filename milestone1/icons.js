1f0a29b5387b39743929d82da1a4a5f589bf2b8e
Mariapia Gianguzzi <piagianguzzi@gmail.com>
2020-12-11 15:59:01 +0100

    js-es6-icons 1 Milestone finito

--- milestone1/icons.js
+++ milestone1/icons.js
@@ -2,14 +2,14 @@ $(document).ready(function () {
   // Creiamo array di oggetti per le icone con le seguenti proprieta: name, prefix, type, family 
   class icons {
     constructor(name, prefix, type, family) {
-      this.name = name; 
-      this.prefix = prefix; 
-      this.type = type; 
-      this.family = family; 
+      this.name = name;
+      this.prefix = prefix;
+      this.type = type;
+      this.family = family;
     }
   }
 
-  const icons = [
+  const icone = [
     new icons("cat", "fas", "fa-cat", "animals"),
     new icons("crow", "fas", "fa-crow", "animals"),
     new icons("dog", "fas", "fa-dog", "animals"),
@@ -18,19 +18,30 @@ $(document).ready(function () {
     new icons("fish", "fas", "fa-fish", "animals"),
     new icons("frog", "fas", "fa-frog", "animals"),
     new icons("hippo", "fas", "fa-hippo", "animals"),
-    new icons("horse", "fas", "fa-horse", "animals"),
+    new icons("horse", "fas", "fa-horse", "animals")
   ];
-  console.log(icons);
+  console.log(icone);
 
-  // Semezioniamo il container icons
+  // Selezioniamo il container icons
+  const container = document.getElementById("icons");
+  
 
   //inseriamo le icone nel container (possiamo creare una funzione tipo print() per inserire gli elementi e richiamarla qui)
-
+icone.forEach(icons => {
+  const markup = `
+    <div>
+        <i class="${icons.prefix} ${icons.type}"></i>
+        <div class= "title">${icons.name.toUpperCase()}</div>
+    </div>
+    `;
+    container.insertAdjacentHTML('beforeend', markup); //incolla il markupp 
+}); 
 });
 
 
 /* ---- FUNCTIONS ----*/
 // Creiamo una funzione per iterare nell'array e che appenda al container le icone.
-// tip: possiamo usare un ciclo foreach qui e destrutturare gli le proprieta degli elementi di un array
-// tip: il template literal ci puo aiutare con il markup
+// tip: possiamo usare un ciclo foreach qui e destrutturare gli le propritype degli elementi di un array
+// tip: il template literal ci puo aiutare con il markup 
+
 
